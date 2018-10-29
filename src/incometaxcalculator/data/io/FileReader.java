be7b73e72543e182d74446c15a3f2d23f2b209ae
ktsiounis <tsiouniskonstantinos@gmail.com>
2018-10-29 20:41:00 +0200

    refactor filereader and subclasses

--- src/incometaxcalculator/data/io/FileReader.java
+++ src/incometaxcalculator/data/io/FileReader.java
@@ -11,68 +11,76 @@ import incometaxcalculator.exceptions.WrongTaxpayerStatusException;
 
 public abstract class FileReader {
 
-  protected abstract int checkForReceipt(BufferedReader inputStream)
-      throws NumberFormatException, IOException;
-
   protected abstract String getValueOfField(String fieldsLine) throws WrongFileFormatException;
+
+  protected abstract int checkSpecificReceipt(String values[]);
 
   public void readFile(String fileName)
       throws NumberFormatException, IOException, WrongTaxpayerStatusException,
       WrongFileFormatException, WrongReceiptKindException, WrongReceiptDateException {
 
-    BufferedReader inputStream = new BufferedReader(new java.io.FileReader(fileName));
-    String fullname = getValueOfField(inputStream.readLine());
-    int taxRegistrationNumber = Integer.parseInt(getValueOfField(inputStream.readLine()));
-    String status = getValueOfField(inputStream.readLine());
-    float income = Float.parseFloat(getValueOfField(inputStream.readLine()));
-    createTaxpayer(fullname, taxRegistrationNumber, income, status);
-    while (readReceipt(inputStream, taxRegistrationNumber))
-      ;
+      BufferedReader inputStream = new BufferedReader(new java.io.FileReader(fileName));
+      String fullname = getValueOfField(inputStream.readLine());
+      int taxRegistrationNumber = Integer.parseInt(getValueOfField(inputStream.readLine()));
+      String status = getValueOfField(inputStream.readLine());
+      float income = Float.parseFloat(getValueOfField(inputStream.readLine()));
+      createTaxpayer(fullname, taxRegistrationNumber, income, status);
+      while (readReceipt(inputStream, taxRegistrationNumber));
   }
 
   protected boolean readReceipt(BufferedReader inputStream, int taxRegistrationNumber)
       throws WrongFileFormatException, IOException, WrongReceiptKindException,
       WrongReceiptDateException {
 
-    int receiptId;
-    if ((receiptId = checkForReceipt(inputStream)) < 0) {
-      return false;
-    }
-    String issueDate = getValueOfField(inputStream.readLine());
-    String kind = getValueOfField(inputStream.readLine());
-    float amount = Float.parseFloat(getValueOfField(inputStream.readLine()));
-    String companyName = getValueOfField(inputStream.readLine());
-    String country = getValueOfField(inputStream.readLine());
-    String city = getValueOfField(inputStream.readLine());
-    String street = getValueOfField(inputStream.readLine());
-    int number = Integer.parseInt(getValueOfField(inputStream.readLine()));
-    createReceipt(receiptId, issueDate, amount, kind, companyName, country, city, street, number,
-        taxRegistrationNumber);
-    return true;
+      int receiptId;
+      if ((receiptId = checkForReceipt(inputStream)) < 0) {
+        return false;
+      }
+      String issueDate = getValueOfField(inputStream.readLine());
+      String kind = getValueOfField(inputStream.readLine());
+      float amount = Float.parseFloat(getValueOfField(inputStream.readLine()));
+      String companyName = getValueOfField(inputStream.readLine());
+      String country = getValueOfField(inputStream.readLine());
+      String city = getValueOfField(inputStream.readLine());
+      String street = getValueOfField(inputStream.readLine());
+      int number = Integer.parseInt(getValueOfField(inputStream.readLine()));
+      createReceipt(receiptId, issueDate, amount, kind, companyName, country, city, street, number,
+          taxRegistrationNumber);
+      return true;
   }
 
   protected void createTaxpayer(String fullname, int taxRegistrationNumber, float income,
       String status) throws WrongTaxpayerStatusException {
 
-    TaxpayerManager manager = new TaxpayerManager();
-    manager.createTaxpayer(fullname, taxRegistrationNumber, status, income);
+      TaxpayerManager manager = new TaxpayerManager();
+      manager.createTaxpayer(fullname, taxRegistrationNumber, status, income);
   }
 
   protected void createReceipt(int receiptId, String issueDate, float amount, String kind,
       String companyName, String country, String city, String street, int number,
       int taxRegistrationNumber) throws WrongReceiptKindException, WrongReceiptDateException {
 
-    TaxpayerManager manager = new TaxpayerManager();
-    manager.createReceipt(receiptId, issueDate, amount, kind, companyName, country, city, street,
-        number, taxRegistrationNumber);
+      TaxpayerManager manager = new TaxpayerManager();
+      manager.createReceipt(receiptId, issueDate, amount, kind, companyName, country, city, street,
+          number, taxRegistrationNumber);
   }
 
   protected boolean isEmpty(String line) {
-    if (line == null) {
-      return true;
-    } else {
-      return false;
-    }
+      if (line == null) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+
+  protected int checkForReceipt(BufferedReader inputStream)
+          throws NumberFormatException, IOException {
+      String line;
+      while (!isEmpty(line = inputStream.readLine())) {
+        String values[] = line.split(" ", 3);
+        return checkSpecificReceipt(values);
+      }
+      return  -1;
   }
 
 }
